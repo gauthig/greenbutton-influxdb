824e57f30d46b07b5f8e83ff69f7248142a43800
gauthig <62046164+gauthig@users.noreply.github.com>
2021-11-10 09:19:15 -0800

    First pass of inluxdb push
    Note - influxdb assumes time is in UTC, this version does not convert your SCE timestamps.
    Next version will convert timestamps using a supplied value and defaulting to -8 as this is made for SCE customers.

--- sceinfluxdb.py
+++ sceinfluxdb.py
@@ -10,8 +10,8 @@ __author__ = "Garrett Gauthier"
 __copyright__ = "Copyright 2021, Garrett Gauthier"
 __credits__ = ["Garrett Gauthier", "Others soon?"]
 __license__ = "GPL"
-__version__ = "1.0.1"
-__VersionDate__ = "9/4/2021"
+__version__ = "0.5.1"
+__VersionDate__ = "11/10/2021"
 __maintainer__ = "gauthig@github"
 __github__  = "https://github.com/gauthig/scegreenbutton"
 __email__ = "garrett-g@outlook.com"
@@ -19,6 +19,7 @@ __status__ = "Production"
 __status__ = "Production"
 
 from influxdb import InfluxDBClient
+#from influxdb.exceptions import InfluxDBClientError
 from pytz import timezone
 import sys, csv, datetime, time, argparse, gzip, json
 
@@ -33,32 +34,46 @@ writer = ''
 metricsout = []
 
 
-def parseData(input_file,orgtimezone):
+def parseData(input_file,orgtimezone,verbose):
     point = []
     rows_generated = 0
     rows_delivered = 0
     row_num = 0
     tag = ''
+    pmult = 0
     infile = open(input_file, mode = 'r')
     csv_reader = csv.reader(infile, delimiter=',')
     for row in csv_reader:
         if len(row) > 0:
-            if 'Received' in row[0]:
+            if "Received" in row[0]:
                 tag = 'generated'
             elif 'Delivered' in row[0]:
-                tag = 'delivered'
+                tag = "delivered"
             elif 'to' in row[0]:
                 times = row[0].split('to')
                 if tag == 'generated':
                     rows_generated = rows_generated + 1
+                    pmult = -1
                 elif tag == 'delivered':
                     rows_delivered = rows_delivered + 1
-                point = {"time": times[1].strip(), "measurement": row[1], "tags": tag}
+                    pmult = 1
+                #point = {"measurement" : "SCE", "tags": tag, "time": times[1].strip(), "field": row[1] }
+                point = [
+                    {
+                    "measurement": "SCE",
+                    "tags": {
+                    "type": tag
+                    },
+                    "time": times[1].strip(),
+                    "fields": {
+                    "value": float(row[1])*pmult
+                    }
+                    }
+                    ]
                 metricsout.append(point)
-                
-                
-                if verbose == 'true' :
+                if verbose :
                     print(point)
+                
 
     return(rows_delivered,rows_generated)
 
@@ -66,16 +81,30 @@ def writedata():
     current_date = datetime.datetime.now()
     fileout = 'load.' + str(int(current_date.strftime("%Y%m%d%H%M"))) + '.out'
     f = open(fileout, mode = 'w')
-        
-
     for t in metricsout:
         f.write(json.dumps(t) + '\n')
-        #f.write(':'.join(str(s) for s in t) + '\n')
-        #print(row)
+
     
     return()
 
-def senddata():
+def senddata(hostname, port,  user, password, dbname, 
+        batchsize, timezone, createdb):
+    client = InfluxDBClient(hostname, port, user, password, dbname)
+
+    if(createdb == True):
+        print('Deleting database %s'%dbname)
+        client.drop_database(dbname)
+        print('Creating database %s'%dbname)
+        client.create_database(dbname)
+
+    if len(metricsout) > 0:
+        #print('Inserting %d metricsout...'%(len(metricsout)))
+        client.switch_user(user, password)
+        for t in metricsout:
+            response = client.write_points(t)
+        #  print("Wrote %d, response: %s" % (len(t), response))
+            
+        
 
     return()
           
@@ -86,29 +115,29 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--hostname", help="the influxdb host name, no port or http\n example\n --host influxdb.mydomain.com")
     parser.add_argument("-v", "--verbose", help="verbose output - send copy of each line to stdout", action="store_true")
     parser.add_argument("-q", "--quiet", help= "do not print totals output", action="store_true")
-    parser.add_argument("-p", "--port", help="port of the influxdb, if not provided it will default to 8086")
+    parser.add_argument("-P", "--port", help="port of the influxdb, if not provided it will default to 8086")
     parser.add_argument("-o", "--csvout", help="sends parsed data to a csvfile.  -p can be used or omitted with -o", action="store_true")
     parser.add_argument('-b', '--batchsize', type=int, default=5000, help='Batch size. Default: 5000.')
     parser.add_argument('--dbname', nargs='?', help='Database name.  Required if -n and -p used')
+    parser.add_argument('-u', '--user', nargs='?', help='influxdb userid')
+    parser.add_argument('-p', '--password', nargs='?', help='Influxdb password')
     parser.add_argument('-tz', '--timezone', default='UTC', help='Timezone of supplied data. Default: UTC')   
-    parser.add_argument('--create', action='store_true', default=False, help='Drop database and create a new one.')    
+    parser.add_argument('--createdb', action='store_true', default=False, help='Drop database and create a new one.')    
     args = parser.parse_args()
 
     if args.version:
             print ('sceinfluxdb.py - version', __version__)
             sys.exit()
 
-
-
-
-    rows_delivered, rows_generated = parseData(args.file, args.timezone)
+    rows_delivered, rows_generated = parseData(args.file, args.timezone,args.verbose)
     
     if args.csvout:
         writedata()
         
     
     if args.hostname:
-        senddata()
+        senddata(args.hostname, args.port,  args.user, args.password, args.dbname, 
+        args.batchsize, args.timezone, args.createdb)
 
     if not args.quiet:
         print("Import Complete")
